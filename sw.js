74a56aeaf3fe8e6d1a807e89b22a8b8a894fbe2f
Dimitrije Kocic <142322539+dida-code@users.noreply.github.com>
2026-05-11 22:07:59 +0200

    Add Service Worker with cache versioning for planer.svg

---
+++ sw.js
@@ -0,0 +1,38 @@
+// sw.js
+const CACHE_VERSION = 'v2-planer-svg';
+const CACHE_NAME = `planer-${CACHE_VERSION}`;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './planer.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
